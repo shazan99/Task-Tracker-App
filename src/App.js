4804c8bc60b226e59b7481792b7653bae5d0db01
shazan99 <shazanahmed786@gmail.com>
2021-08-24 02:28:16 +0530

    Feature added-> Delete Task & prop drilling,show message if there is no task,Add tAsk form, form input state, Added submit task button, showAddTask state, button toggle color
    Signed-off-by: shazan99 <shazanahmed786@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () =>{
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -25,6 +27,13 @@ const App = () =>{
     },
   ])
 
+// Add Task
+const addTask = (task) => {
+  const id= Math.floor(Math.random() * 10000)+1;
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
   //Delete Task
   const deleteTask =(id) => {
     setTasks(tasks.filter((task) => task.id !==id))
@@ -32,15 +41,18 @@ const App = () =>{
 
   //Toggle Reminder
   const toggleReminder = (id) => {
-    console.log(id);
+    setTasks(tasks.map((task) => task.id === id 
+    ? {...task, reminder: !task.reminder} : task))
   }
 
   return (
     <div className='container'>
-     <Header /> 
+     <Header onAdd={ () => setShowAddTask
+       (!showAddTask)}
+        showAdd={showAddTask}/> 
+     {showAddTask && <AddTask onAdd={addTask}/>}
      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} 
-     onToggle={toggleReminder} />
-      : 'No Tasks Exists'}
+     onToggle={toggleReminder} /> : 'No Tasks Exists'}
     
     </div>
   );
