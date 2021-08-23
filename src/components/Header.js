4efcc09d745aca61867f0945839ac5dce1da9305
shazan99 <shazanahmed786@gmail.com>
2021-08-24 04:12:02 +0530

    JSON server, useEffect Hook and Fetch Task, Delete & Add Task from server, Toggle reminder on Server, Routing, footer and about section
    Signed-off-by: shazan99 <shazanahmed786@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
 const Header = ({ title, onAdd, showAdd }) => {
 
+    const location = useLocation()
     return (
         <header className='header'>
          <h1>{title} </h1>
+        {location.pathname ==='/' && 
         <Button color ={showAdd ? 'red' : 'gold'} 
-        text ={showAdd ? 'Close': 'Add'} onClick= {onAdd} /> 
+        text ={showAdd ? 'Close': 'Add'} onClick= {onAdd} />} 
         </header>
     )
 }
